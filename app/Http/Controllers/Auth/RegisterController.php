0d311ceded0de0bed7ba61739d90e7243dfaed35
Illiyan Ainun Qalbi <nullroku@gmail.com>
2017-04-22 20:02:03 +0800

    Penmabahan kolom status pada tabel users

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'status' => 'incomplete',
         ]);
     }
 }
